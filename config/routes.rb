636bb5fa75a1d76e3c78f1e44a7214763945cd9c
sema karan <karan18sema@gmail.com>
2024-02-19 11:53:05 +0100

    lesson completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # GET localhost:3000/about
+  #  get request path to the page - controller#action name
+  #  verb "/path" to: "controller/action"
+  # same way as below -> get("/about", { to: "pages#about" })
+
+  # how to route to home without /home.. -> get '/', to: 'pages#home'
+  root to: 'pages#home'
+  get '/about', to: 'pages#about'
+
+  get '/contact', to: 'pages#contact'
 end
